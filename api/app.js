15be13b3afb7e99cba12e012c78dc79d923b10de
JoseDanielMX <jduvmx@gmail.com>
2020-11-11 17:57:10 -0600

    Created an API endpoint to return list of posts

--- api/app.js
+++ api/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send("Hello world!")
+const posts = [{
+    "id": "1581461442206",
+    "title": "Title",
+    "description": "Description",
+    "content": "Content",
+    "post_image": "uploads/post-image-1581461442199.jpg",
+    "added_date": "1581461442206"
+}]
+
+app.get("/api/posts", (req, res) => {
+    res.status(200).send(posts);
 });
 
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
