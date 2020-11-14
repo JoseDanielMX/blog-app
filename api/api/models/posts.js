c3b0ffcf7bd34751a275f69bba58e578537855b2
JoseDanielMX <jduvmx@gmail.com>
2020-11-13 19:04:01 -0600

    Created readData function to get data from data.json

--- api/api/models/posts.js
+++ api/api/models/posts.js
@@ -1,12 +1,22 @@
+const PATH = "./data.json";
+const fs = require('fs');
+
 class Post {
     get() {
         /** Get posts */
+        return this.readData();
     }
     getIndividualBlog() {
         /** Get one blog post */
     }
     add() {
         /** Add new post */
+    }
+    readData() {
+        /** Get data from data.json */
+        let rawdata = fs.readFileSync(PATH);
+        let posts = JSON.parse(rawdata);
+        return posts;
     }
 }
 
--- api/app.js
+++ api/app.js
@@ -7,17 +7,8 @@ app.use(helmet());
 const Post = require("./api/models/posts");
 const postsData = new Post();
 
-const posts = [{
-    "id": "1581461442206",
-    "title": "Title",
-    "description": "Description",
-    "content": "Content",
-    "post_image": "uploads/post-image-1581461442199.jpg",
-    "added_date": "1581461442206"
-}]
-
 app.get("/api/posts", (req, res) => {
-    res.status(200).send(posts);
+    res.status(200).send(postsData.get());
 });
 
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
