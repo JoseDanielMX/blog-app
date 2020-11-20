74f70aaea6f166038fdd01960b01fb963164e95f
JoseDanielMX <jduvmx@gmail.com>
2020-11-20 15:52:41 -0600

    Created API endpoint for adding a new post without image upload

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,8 @@ app.use(helmet());
 const Post = require("./api/models/posts");
 const postsData = new Post();
 
+app.use(express.json());
+
 app.use("/uploads", express.static("uploads"));
 
 app.get("/api/posts", (req, res) => {
@@ -22,5 +24,18 @@ app.get("/api/posts/:post_id", (req, res) => {
         res.status(404).send("Not found");
     }
 });
+
+app.post("/api/posts", (req, res) => {
+    const newPost = {
+        "id": `${Date.now()}`,
+        "added_date": `${Date.now()}`,
+        "post_image": req.body["post-image"],
+        "title": req.body.title,
+        "description": req.body.description,
+        "content": req.body.content
+    }
+    postsData.add(newPost);
+    res.status(201).send(newPost);
+})
 
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
